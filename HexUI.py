1e89e4fb533062c2b16a7e526879b1545db73e63
Max <maxfooo@gmail.com>
2016-01-24 15:34:14 -0700

    Create HexUI.py

---
+++ HexUI.py
@@ -0,0 +1,77 @@
+'''
+Created on Jan 24, 2016
+
+@author: Max Ruiz
+'''
+from tkinter import *
+from FileIO import *
+
+class HexUI(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        master.title('Hex Parameters')
+        self.pack()
+
+        self.parameters = ['depth','width','address_radix','data_radix']
+
+        self.fileIO = FileIO()
+
+        self.mainFrame()
+
+    def mainFrame(self):
+
+        depthFrame = Frame(self)
+        depthValue = StringVar()
+        depthLabel = Label(depthFrame, text = 'Mem Depth')
+        depthLabel.pack(side = 'left')
+        self.depthEntry = Entry(depthFrame, textvariable = depthValue)
+        self.depthEntry.pack(side = 'left')
+        depthFrame.pack()
+
+        widthFrame = Frame(self)
+        widthValue = StringVar()
+        widthLabel = Label(widthFrame, text = 'Mem width')
+        widthLabel.pack(side = 'left')
+        self.widthEntry = Entry(widthFrame, textvariable = widthValue)
+        self.widthEntry.pack(side = 'left')
+        widthFrame.pack()
+
+        addRadFrame = Frame(self)
+        addRadValue = StringVar()
+        addRadLabel = Label(addRadFrame, text = 'Mem addRad')
+        addRadLabel.pack(side = 'left')
+        self.addRadEntry = Entry(addRadFrame, textvariable = addRadValue)
+        self.addRadEntry.pack(side = 'left')
+        addRadFrame.pack()
+
+        datRadFrame = Frame(self)
+        datRadValue = StringVar()
+        datRadLabel = Label(datRadFrame, text = 'Mem datRad')
+        datRadLabel.pack(side = 'left')
+        self.datRadEntry = Entry(datRadFrame, textvariable = datRadValue)
+        self.datRadEntry.pack(side = 'left')
+        datRadFrame.pack()
+
+        setParamButton = Button(self, text = 'Set Parameters', command = self.getFieldEntries)
+        setParamButton.pack()
+
+    def getFieldEntries(self):
+        try:
+            self.parameters[0] = int(self.depthEntry.get())
+            self.parameters[1] = int(self.widthEntry.get())
+            self.parameters[2] = self.addRadEntry.get()
+            self.parameters[3] = self.datRadEntry.get()
+            self.quit()
+        except:
+            self.fileIO.errorPopup('Depth and Width should be integers!')
+
+    def getParameters(self):
+        return self.parameters
+
+        '''
+        depth
+        width
+        address_radix
+        data_radix
+        '''
