35d5e34dbb5de6ef9e5e4bb7841f4a40c70c7565
Max <maxfooo@gmail.com>
2016-06-23 16:53:32 -0700

    Update HEXClass.py

--- HEXClass.py
+++ HEXClass.py
@@ -4,6 +4,7 @@ Created on Jan 21, 2016
 @author: Max Ruiz
 '''
 import re
+from testing import tempAddress
 
 class HEXClass(object):
     '''
@@ -33,7 +34,7 @@ class HEXClass(object):
         self.address_radix = address_radix
         self.data_radix = data_radix
         self.data_digits = self.base / width
-        self.fullZeros = fillZeros
+        self.fillZeros = fillZeros
 
     def convertHEXtoMIF(self):
         self.sortHEXContents()
@@ -50,7 +51,7 @@ class HEXClass(object):
     def sortHEXContents(self):
         hex_format_expression = r':(\w{2})(\w{4})(\w{2})(\w*)(\w{2})'
         hexFields = re.findall(hex_format_expression,self.hexContent)
-
+        
         for records in hexFields:
             self.hexLen.append(records[0])
             self.hexAddr.append(records[1])
@@ -60,27 +61,34 @@ class HEXClass(object):
             else:
                 self.hexData.append(re.findall(r'\w{%d}' %self.data_digits,records[3]))
             self.hexCS.append(records[4])
+                
 
     def writeMIFFile(self):
         self.mifFile.write('DEPTH = {};\nWIDTH = {};\n'.format(self.depth,self.width) +
                            'ADDRESS_RADIX = {};\n'.format(self.address_radix) +
                            'DATA_RADIX = {};\n'.format(self.data_radix) +
                            'CONTENT\nBEGIN\n')
+        
 
+        self.mifLineCount = 0
         for i in range(len(self.hexLen)):
             tempAddress = []
             for j in range(int(self.hexLen[i],self.base)):
-                tempAddress.append(int(self.hexAddr[i],self.base)+j)
+                addr = int(self.hexAddr[i],self.base)+j
+                tempAddress.append(addr)
+                if addr > self.mifLineCount:
+                    self.mifLineCount = addr
             for k in range(len(tempAddress)):
                 self.mifFile.write(str(hex(tempAddress[k])).replace('0x','') +
                                    '\t:\t' + str(self.hexData[i][k]) + ';\n')
-                
+        
+        
         # Fill in the rest of the addresses with 00
-        if len(self.hexLen) < int(self.depth) and self.fillZeros == 1:
-            for i in range(int(self.depth) - len(self.hexLen)):
-                    self.mifFile.write(str(hex(len(self.hexLen) + i)).replace('0x','') +
+        if self.mifLineCount < int(self.depth) and self.fillZeros == 1:
+            for i in range(int(self.depth) - self.mifLineCount):
+                    self.mifFile.write(str(hex(self.mifLineCount + i + 1)).replace('0x','') +
                                    '\t:\t' + '00' + ';\n')
-                    
+        
         self.mifFile.write('END;')
 
 
